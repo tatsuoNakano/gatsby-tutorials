3f1d1a44bfae38c617dfafe64c1d0f5ddfcf8019
tatsuoNakano <aaaoauo1118@gmeil.com>
2024-02-05 19:55:11 +0900

    1-4終了

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
-import Layout from "../components/layout,js";
+import Layout from "../components/layout";
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -12,10 +13,11 @@ const AboutPage = () => {
 }
 export const Head = () => (
     <>
-        <title>About Me</title>
+        <Seo title="About Me" />
         <meta name="description" content="Your description" />
     </>
 )
+
 
 // Step 3: Export your component
 export default AboutPage
